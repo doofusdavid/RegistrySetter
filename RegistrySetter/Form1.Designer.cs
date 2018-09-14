dd324d6afeeef4b2b9906e3ff9da37b33d555262
David Edwards <david.edwards@gmail.com>
2018-09-14 12:28:35 -0600

    created

--- RegistrySetter/Form1.Designer.cs
+++ RegistrySetter/Form1.Designer.cs
@@ -28,13 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSetRegistry = new System.Windows.Forms.Button();
+            this.cboSelectList = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSetRegistry
+            // 
+            this.btnSetRegistry.Location = new System.Drawing.Point(438, 256);
+            this.btnSetRegistry.Name = "btnSetRegistry";
+            this.btnSetRegistry.Size = new System.Drawing.Size(167, 79);
+            this.btnSetRegistry.TabIndex = 0;
+            this.btnSetRegistry.Text = "button1";
+            this.btnSetRegistry.UseVisualStyleBackColor = true;
+            this.btnSetRegistry.Click += new System.EventHandler(this.btnSetRegistry_Click);
+            // 
+            // cboSelectList
+            // 
+            this.cboSelectList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSelectList.FormattingEnabled = true;
+            this.cboSelectList.Items.AddRange(new object[] {
+            "One Item",
+            "Another Item",
+            "Third"});
+            this.cboSelectList.Location = new System.Drawing.Point(452, 115);
+            this.cboSelectList.Name = "cboSelectList";
+            this.cboSelectList.Size = new System.Drawing.Size(230, 28);
+            this.cboSelectList.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(80, 172);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(51, 20);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cboSelectList);
+            this.Controls.Add(this.btnSetRegistry);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSetRegistry;
+        private System.Windows.Forms.ComboBox cboSelectList;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- RegistrySetter/Form1.cs
+++ RegistrySetter/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace RegistrySetter
 {
@@ -15,6 +16,33 @@ namespace RegistrySetter
         public Form1()
         {
             InitializeComponent();
+            lblStatus.Text = "Ready to Set Values";
+        }
+
+        private void btnSetRegistry_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(cboSelectList.SelectedItem.ToString()))
+            {
+                SetRegistry(cboSelectList.SelectedItem.ToString());
+            }
+        }
+
+        private void SetRegistry(string toString)
+        {
+            // The name of the key must include a valid root.
+            const string userRoot = "HKEY_CURRENT_USER";
+            const string subkey = "RegistrySetValueExample";
+            const string keyName = userRoot + "\\" + subkey;
+
+            // An int value can be stored without specifying the
+            // registry data type, but long values will be stored
+            // as strings unless you specify the type. Note that
+            // the int is stored in the default name/value
+            // pair.
+
+            // uncomment to actually save
+            //Registry.SetValue(keyName, "Bob", toString, RegistryValueKind.String);
+            lblStatus.Text = "Registry Value: " + toString + " Saved!";
         }
     }
 }
